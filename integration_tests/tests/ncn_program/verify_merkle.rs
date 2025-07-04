5793be588d003ba8c80f26cbaaabc019aca278cb
iurage <iurage@protonmail.com>
2025-07-05 05:23:50 +0800

    Add proof gen checks

--- integration_tests/tests/ncn_program/verify_merkle.rs
+++ integration_tests/tests/ncn_program/verify_merkle.rs
@@ -1,11 +1,20 @@
 #[cfg(test)]
 mod tests {
-    use crate::fixtures::ncn_program_client::assert_ncn_program_error;
+    use crate::fixtures::assert_ix_error;
+    use crate::fixtures::ncn_program_client::{assert_ncn_program_error, NCNProgramClient};
+    use crate::fixtures::test_builder::TestNcn;
     use crate::fixtures::{test_builder::TestBuilder, TestResult};
     use jito_restaking_core::{config::Config, ncn_vault_ticket::NcnVaultTicket};
-    use ncn_program_client::types::MetaMerkleSnapshot;
+    use meta_merkle_tree::merkle_tree::MerkleTree;
+    use ncn_program_client::types::{MetaMerkleSnapshot, StakeMerkleLeaf};
     use ncn_program_core::error::NCNProgramError;
-    use solana_sdk::{hash::hash, signature::Keypair, signer::Signer};
+    use solana_sdk::instruction::InstructionError;
+    use solana_sdk::pubkey::Pubkey;
+    use solana_sdk::{
+        hash::{hash, hashv, Hash},
+        signature::Keypair,
+        signer::Signer,
+    };
     use std::fs::File;
     use std::io::BufReader;
 
@@ -13,6 +22,99 @@ mod tests {
         let file = File::open(path).expect("Failed to open file");
         let reader = BufReader::new(file);
         serde_json::from_reader(reader).expect("Failed to deserialize")
+    }
+
+    fn get_stake_merkle_proof(
+        stake_merkle_leaves: Vec<StakeMerkleLeaf>,
+        index: usize,
+    ) -> Vec<[u8; 32]> {
+        pub fn hash(leaf: &StakeMerkleLeaf) -> Hash {
+            hashv(&[
+                &leaf.voting_wallet.to_bytes(),
+                &leaf.stake_account.to_bytes(),
+                &leaf.active_stake.to_le_bytes(),
+            ])
+        }
+
+        fn get_proof(merkle_tree: &MerkleTree, index: usize) -> Vec<[u8; 32]> {
+            let mut proof = Vec::new();
+            let path = merkle_tree.find_path(index).expect("path to index");
+            for branch in path.get_proof_entries() {
+                if let Some(hash) = branch.get_left_sibling() {
+                    proof.push(hash.to_bytes());
+                } else if let Some(hash) = branch.get_right_sibling() {
+                    proof.push(hash.to_bytes());
+                } else {
+                    panic!("expected some hash at each level of the tree");
+                }
+            }
+            proof
+        }
+
+        let hashed_nodes: Vec<[u8; 32]> = stake_merkle_leaves
+            .iter()
+            .map(|n| hash(n).to_bytes())
+            .collect();
+        let stake_merkle = MerkleTree::new(&hashed_nodes[..], true);
+        get_proof(&stake_merkle, index)
+    }
+
+    async fn cast_votes(
+        fixture: &mut TestBuilder,
+        test_ncn: &TestNcn,
+        ncn_program_client: &mut NCNProgramClient,
+        ncn_pubkey: Pubkey,
+        merkle_root: [u8; 32],
+        snapshot_hash: [u8; 32],
+    ) -> TestResult<()> {
+        let epoch = fixture.clock().await.epoch;
+        for operator_root in test_ncn.operators.iter() {
+            let operator = operator_root.operator_pubkey;
+
+            ncn_program_client
+                .do_cast_vote(
+                    ncn_pubkey,
+                    operator,
+                    &operator_root.operator_admin,
+                    merkle_root,
+                    snapshot_hash,
+                    epoch,
+                )
+                .await?;
+        }
+
+        Ok(())
+    }
+
+    async fn setup_voting_epoch(
+        fixture: &mut TestBuilder,
+        test_ncn: &mut TestNcn,
+        ncn_program_client: &mut NCNProgramClient,
+    ) -> TestResult<()> {
+        // Initialize the epoch state - creates a new state for the current epoch
+        fixture.add_epoch_state_for_test_ncn(test_ncn).await?;
+
+        // Initialize the weight table - prepares the table that will track voting weights
+        let clock = fixture.clock().await;
+        let epoch = clock.epoch;
+        ncn_program_client
+            .do_full_initialize_weight_table(test_ncn.ncn_root.ncn_pubkey, epoch)
+            .await?;
+
+        // Take staking token weight snapshot, epoch snapshot, operator snapshots and vault snapshots.
+        ncn_program_client
+            .do_set_epoch_weights(test_ncn.ncn_root.ncn_pubkey, epoch)
+            .await?;
+        fixture.add_epoch_snapshot_to_test_ncn(test_ncn).await?;
+        fixture.add_operator_snapshots_to_test_ncn(test_ncn).await?;
+        fixture
+            .add_vault_operator_delegation_snapshots_to_test_ncn(test_ncn)
+            .await?;
+
+        // Initialize the ballot box - creates the voting container for this epoch
+        fixture.add_ballot_box_to_test_ncn(test_ncn).await?;
+
+        Ok(())
     }
 
     #[tokio::test]
@@ -47,7 +149,6 @@ mod tests {
 
         // 2.b. Initialize the operators using the Jito Restaking program, and initiate the
         //   handshake relationship between the NCN <> operators
-        // Creates OPERATOR_COUNT operators and associates them with the NCN.
         fixture
             .add_operators_to_test_ncn(&mut test_ncn, OPERATOR_COUNT, Some(operator_fee_bps))
             .await?;
@@ -59,7 +160,6 @@ mod tests {
             .await?;
 
         // 2.d. Vaults delegate stakes to operators
-        // Each vault delegates different amounts to different operators based on the delegation amounts array
         for operator_root in &test_ncn.operators {
             vault_program_client
                 .do_add_delegation(
@@ -72,7 +172,6 @@ mod tests {
         }
 
         // 2.e Fast-forward time to simulate a full epoch passing
-        // This is needed for all the relationships to finish warming up
         let restaking_config_address =
             Config::find_program_address(&jito_restaking_program::id()).0;
         let restaking_config = restaking_client
@@ -85,8 +184,6 @@ mod tests {
             .unwrap();
 
         // 3. Setting up the NCN-program
-        // every thing here will be a call for an instruction to the NCN program that the NCN admin
-        // is suppose to deploy to the network.
         {
             // 3.a. Initialize the config for the ncn-program
             ncn_program_client
@@ -121,106 +218,102 @@ mod tests {
             }
         }
 
-        // At this point, all the preparations and configurations are done, everything else after
-        // this is part of the consensus cycle, so it depends on the way you setup your voting system
-        // you will have to run the code below
-        //
-        // in this example, the voting is cyclical, and per epoch, so the code you will see below
-        // will run per epoch to prepare for the voting
-
         // 4. Prepare the voting environment
-        {
-            // 4.a. Initialize the epoch state - creates a new state for the current epoch
-            fixture.add_epoch_state_for_test_ncn(&test_ncn).await?;
-            // 4.b. Initialize the weight table - prepares the table that will track voting weights
-            let clock = fixture.clock().await;
-            let epoch = clock.epoch;
+        let epoch1 = fixture.clock().await.epoch;
+        setup_voting_epoch(&mut fixture, &mut test_ncn, &mut ncn_program_client).await?;
+        cast_votes(
+            &mut fixture,
+            &test_ncn,
+            &mut ncn_program_client,
+            ncn_pubkey,
+            meta_merkle_snapshot.root,
+            hash(b"snapshot1").to_bytes(),
+        )
+        .await?;
+
+        // 6. Verify for some vote accounts
+        for i in 0..10 {
+            let bundle = &meta_merkle_snapshot.leaf_bundles[i * 10];
+
+            let meta_proof = bundle.proof.clone().unwrap();
+            let meta_leaf = &bundle.meta_merkle_leaf;
+
             ncn_program_client
-                .do_full_initialize_weight_table(test_ncn.ncn_root.ncn_pubkey, epoch)
+                .do_verify_merkle(
+                    ncn_pubkey,
+                    epoch1,
+                    meta_proof.clone(),
+                    meta_leaf.clone(),
+                    None,
+                    None,
+                )
                 .await?;
 
-            // 4.c. Take a snapshot of the weights for each ST mint
-            // This records the current weights for the voting calculations
-            ncn_program_client
-                .do_set_epoch_weights(test_ncn.ncn_root.ncn_pubkey, epoch)
-                .await?;
-            // 4.d. Take the epoch snapshot - records the current state for this epoch
-            fixture.add_epoch_snapshot_to_test_ncn(&test_ncn).await?;
-            // 4.e. Take a snapshot for each operator - records their current stakes
-            fixture
-                .add_operator_snapshots_to_test_ncn(&test_ncn)
-                .await?;
-            // 4.f. Take a snapshot for each vault and its delegation - records delegations
-            fixture
-                .add_vault_operator_delegation_snapshots_to_test_ncn(&test_ncn)
-                .await?;
-
-            // 4.g. Initialize the ballot box - creates the voting container for this epoch
-            fixture.add_ballot_box_to_test_ncn(&test_ncn).await?;
-        }
-
-        let winning_merkle = meta_merkle_snapshot.root;
-        let winning_snapshot = hash(b"snapshot1").to_bytes();
-
-        // 5. Cast votes from operators
-        let epoch = fixture.clock().await.epoch;
-        {
-            for operator_root in test_ncn.operators.iter() {
-                let operator = operator_root.operator_pubkey;
-
+            // Verify for stake accounts under this vote account.
+            let stake_leaves = &bundle.stake_merkle_leaves;
+            for (j, stake_leaf) in stake_leaves.iter().take(5).enumerate() {
+                let stake_proof = get_stake_merkle_proof(stake_leaves.clone(), j);
                 ncn_program_client
-                    .do_cast_vote(
+                    .do_verify_merkle(
                         ncn_pubkey,
-                        operator,
-                        &operator_root.operator_admin,
-                        winning_merkle,
-                        winning_snapshot,
-                        epoch,
+                        epoch1,
+                        meta_proof.clone(),
+                        meta_leaf.clone(),
+                        Some(stake_proof),
+                        Some(stake_leaf.clone()),
                     )
                     .await?;
             }
         }
 
-        // 6. Verify for some vote accounts
-        for i in 0..10 {
-            let merkle_leaf_bundle = meta_merkle_snapshot.leaf_bundles[i * 10].clone();
-
-            let meta_merkle_proof = merkle_leaf_bundle.proof.unwrap();
-            let meta_merkle_leaf = merkle_leaf_bundle.meta_merkle_leaf;
-            ncn_program_client
-                .do_verify_merkle(
-                    ncn_pubkey,
-                    epoch,
-                    meta_merkle_proof.clone(),
-                    meta_merkle_leaf.clone(),
-                    None,
-                    None,
-                )
-                .await?;
-        }
-
         // 7. Expect failure for wrong proofs.
         for i in 0..10 {
-            let merkle_leaf_bundle1 = meta_merkle_snapshot.leaf_bundles[i + 5].clone();
-            let merkle_leaf_bundle2 = meta_merkle_snapshot.leaf_bundles[i + 3].clone();
+            let bundle1 = &meta_merkle_snapshot.leaf_bundles[i + 5];
+            let bundle2 = &meta_merkle_snapshot.leaf_bundles[i + 3];
 
-            let meta_merkle_proof = merkle_leaf_bundle1.proof.unwrap();
-            let meta_merkle_leaf = merkle_leaf_bundle2.meta_merkle_leaf;
+            let meta_leaf = &bundle1.meta_merkle_leaf;
+            let wrong_proof = bundle2.proof.clone().unwrap();
+
             let result = ncn_program_client
                 .do_verify_merkle(
                     ncn_pubkey,
-                    epoch,
-                    meta_merkle_proof.clone(),
-                    meta_merkle_leaf.clone(),
+                    epoch1,
+                    wrong_proof,
+                    meta_leaf.clone(),
                     None,
                     None,
                 )
                 .await;
             assert_ncn_program_error(result, NCNProgramError::InvalidProof, Some(0));
+
+            let stake_leaves = &bundle1.stake_merkle_leaves;
+            let proof = bundle1.proof.clone().unwrap();
+
+            for (j, stake_leaf) in stake_leaves.iter().take(5).enumerate() {
+                let mut stake_proof = get_stake_merkle_proof(stake_leaves.clone(), j);
+                stake_proof.remove(0);
+                let result = ncn_program_client
+                    .do_verify_merkle(
+                        ncn_pubkey,
+                        epoch1,
+                        proof.clone(),
+                        meta_leaf.clone(),
+                        Some(stake_proof),
+                        Some(stake_leaf.clone()),
+                    )
+                    .await;
+                assert_ncn_program_error(result, NCNProgramError::InvalidProof, Some(0));
+            }
         }
 
-        // TODO: Test proof max size
-        // TODO: Test invalid consensus result
+        // Test invalid consensus result
+        let bundle = &meta_merkle_snapshot.leaf_bundles[0];
+        let meta_proof = bundle.proof.clone().unwrap();
+        let meta_leaf = bundle.meta_merkle_leaf.clone();
+        let transaction_error = ncn_program_client
+            .do_verify_merkle(ncn_pubkey, epoch1 - 2, meta_proof, meta_leaf, None, None)
+            .await;
+        assert_ix_error(transaction_error, InstructionError::ProgramFailedToComplete);
 
         Ok(())
     }
